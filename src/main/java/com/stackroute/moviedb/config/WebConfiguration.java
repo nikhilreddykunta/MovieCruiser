4726e634a0db313d500d73a146c07c3841f4bb93
nikhilreddykunta <kunta.nike.n@gmail.com>
2018-10-08 22:37:52 +0530

    added mysql instead of h2

--- src/main/java/com/stackroute/moviedb/config/WebConfiguration.java
+++ src/main/java/com/stackroute/moviedb/config/WebConfiguration.java
@@ -1,6 +1,6 @@
 package com.stackroute.moviedb.config;
 
-import org.h2.server.web.WebServlet;
+
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,12 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class WebConfiguration {
-    @Bean
-    ServletRegistrationBean h2servletRegistration(){
-        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
-        registrationBean.addUrlMappings("/console/*");
-        return registrationBean;
-    }
+
 
     @Bean
     public Docket api() {
